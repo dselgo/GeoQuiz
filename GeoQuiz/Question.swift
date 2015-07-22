097a0b787b2cf19973797a8a983dc4b066d806c4
Danny <dselgo@mojados-Mac-2.local>
2015-07-22 15:22:29 -0400

    Added shuffle, timer, and answer checking functions to Question.swift

--- GeoQuiz/Question.swift
+++ GeoQuiz/Question.swift
@@ -18,6 +18,9 @@ class Question {
     var question: String
     var answers: [Answer] = [Answer(), Answer(), Answer(), Answer()]
     var image: PFFile! = nil
+    var timer = NSTimer()
+    var counter: Double = 10.0
+    var decrementTime: Double = 0.1
     
     init(question: String, answer1: String, answer2: String, answer3: String, answer4: String, correctAnswer: Int){
         self.question = question
@@ -37,10 +40,44 @@ class Question {
         default:
             break
         }
+        answers.shuffle()
     }
     
     convenience init(question: String, answer1: String, answer2: String, answer3: String, answer4: String, correctAnswer: Int, image: PFFile){
         self.init(question: question, answer1: answer1, answer2: answer2, answer3: answer3, answer4: answer4, correctAnswer: correctAnswer)
         self.image = image
+    }
+    
+    func checkAnswer(guess: Int) -> Bool{
+        return answers[guess - 1].isCorrect
+    }
+    
+    func startTimer(){
+        timer = NSTimer.scheduledTimerWithTimeInterval(decrementTime, target:self, selector: Selector("updateTimer"), userInfo: nil, repeats: false)
+    }
+    
+    func updateTimer(){
+        counter -= decrementTime
+        //add code to update label here
+        if(counter <= 0){
+            stopTimer()
+            //add code to stop quiz
+        }
+    }
+    
+    func stopTimer(){
+        timer.invalidate()
+    }
+}
+
+extension Array {
+    func shuffle() -> [T] {
+        if count < 2 { return self }
+        var list = self
+        for i in 0..<(list.count - 1) {
+            let j = Int(arc4random_uniform(UInt32(list.count - i))) + i
+            swap(&list[i], &list[j])
+        }
+        return list
     }
 }
