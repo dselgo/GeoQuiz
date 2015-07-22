44285fa12eca2ccf86318941ce921af49bba41b4
gaberex <kalee@stut.org>
2015-07-22 15:41:42 -0400

    added all View Controller classes

---
+++ QuestionViewController.swift
@@ -0,0 +1,42 @@
+//
+//  QuestionViewController.swift
+//  GeoQuiz
+//
+//  Created by Deforation on 7/22/15.
+//  Copyright (c) 2015 Team F. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    @IBOutlet weak var answer1Button: UIButton!
+    @IBOutlet weak var answer2Button: UIButton!
+    @IBOutlet weak var answer3Button: UIButton!
+    @IBOutlet weak var answer4Button: UIButton!
+    
+    let borderSize : CGFloat = 0.7
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        answer1Button.layer.borderWidth = 1.0
+        answer1Button.layer.borderColor = UIColor(white: 0.0, alpha: borderSize).CGColor
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
